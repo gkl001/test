5f6f7bb348a8f90f870803d9b57422c973dd5666
gkl001 <60808416+gkl001@users.noreply.github.com>
2020-02-09 11:25:17 +0800

    Update and rename dubbo_common/src/main/java/com/jk/model/CarModel.java to 杜博公共/SRC/主/爪哇/COM/JK/模型/CarModel.java
    tijiao

--- 杜博公共/SRC/主/爪哇/COM/JK/模型/CarModel.java
+++ 杜博公共/SRC/主/爪哇/COM/JK/模型/CarModel.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 public class CarModel implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    private String lml;
     private Integer carid;
 
     private String carname;
